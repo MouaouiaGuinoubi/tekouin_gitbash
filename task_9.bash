9480f50d7c1f61647ffd3d4af6e6a1df2f515786
MouaouiaGuinoubi <Mouawiya.Guinoubi@wimbeetech.comm>
2024-02-08 15:23:05 +0100

    Task 9

---
+++ task_9.bash
@@ -0,0 +1,12 @@
+
+while [ $line in (( ls /etc/passwd )) ];
+do
+    USERNAME=awk -F  ':' '{print $1}'
+    PASSWORD=awk -F  ':' '{print $2}'
+    USER_ID=awk -F  ':' '{print $3}'
+    GROUP_ID=awk -F  ':' '{print $4}'
+    USER_ID_INFO=awk -F  ':' '{print $5}'
+    HOME_DIRECTORY=awk -F  ':' '{print $6}'
+    COMMAND=awk -F  ':' '{print $7}'
+    echo "The user $USERNAME is part of the $GROUP_ID gang, lives in $HOME_DIRECTORY and rides $COMMAND. $USER_ID's place is protected by the passcode $PASSWORD, more info about the user here: $USER_ID_INFO"
+done
